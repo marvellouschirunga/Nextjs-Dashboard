d01c827b0941b7b8992425c3dac57a75ae1ef062
Marvellous Humphery Chirunga <marvellouschirunga@gmail.com>
2024-03-24 05:27:57 +0200

    Handling errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,15 +15,12 @@ export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
-
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
+
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -170,7 +167,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
